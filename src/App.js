8100474ab1aec0666b711c83a2b927c2f1ed1b91
RJS025 <waveryder.25@gmail.com>
2019-02-16 22:14:33 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Quote from 'inspirational-quotes';
 
 class App extends Component {
+  
+  state = {
+    text: Quote.getQuote().text,
+    author: Quote.getQuote().author
+  }
+
+  newQuoteHandler = () => {
+    const newText = Quote.getQuote().text;
+    const newAuthor = Quote.getQuote().author;
+    this.setState({
+      text: newText,
+      author: newAuthor
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="quote-box">
+        <p id="text">{this.state.text}</p>
+        <p id="author">{this.state.author}</p>
+        <button id="new-quote" onClick={this.newQuoteHandler}>GiveMeQuotes</button>
+        <button id="teewt-quote">Tweet</button>
       </div>
     );
   }
