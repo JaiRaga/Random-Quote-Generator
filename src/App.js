9af69d8ffa042d66e9cea281bdd66e5c933134ee
RJS025 <waveryder.25@gmail.com>
2019-02-21 11:48:09 +0530

    Added Styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Quote from 'inspirational-quotes';
 
+import Quotes from './components/Quotes';
+
 class App extends Component {
   
   state = {
@@ -19,12 +21,26 @@ class App extends Component {
   }
 
   render() {
+    const bgC = ["rgb(71, 46, 50)", "rgb(189, 187, 153)", "rgb(231, 76, 60)", "rgb(251, 105, 100)", "rgb(44, 62, 80)", "rgb(115, 168, 87)"] 
+    const v = Math.floor(Math.random() * (bgC.length - 1))
+
+    const style = {
+      backgroundColor: bgC[v],
+      color: bgC[v]
+    }
+
+    const buttonStyle = {
+      backgroundColor: bgC[v],
+      color: 'white'
+    }
+
     return (
-      <div id="quote-box">
-        <p id="text">{this.state.text}</p>
-        <p id="author">{this.state.author}</p>
-        <button id="new-quote" onClick={this.newQuoteHandler}>GiveMeQuotes</button>
-        <button id="teewt-quote">Tweet</button>
+      <div id="quote-box" style={style}>
+        <Quotes
+          text = {this.state.text}
+          author = {this.state.author} 
+          click = {this.newQuoteHandler}
+          s = {buttonStyle} />
       </div>
     );
   }
---
+++ src/components/Quotes.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import '../../src/App.css'
+
+const quote = (props) => {
+    return (
+        <div className = "quotebox">
+           <p id="text">{props.text}</p>
+           <p id="author">---{props.author}---</p>
+           <button 
+             id="new-quote" 
+             onClick={props.click} 
+             style={props.s}>GiveMeQuotes</button>
+
+           <button class="tweet"><a href = "twitter.com/intent/tweet" id="tweet-quote" style={props.s} target="_blank" title="Tweet this quote"><i class="fa fa-twitter"></i></a></button>
+        </div>
+    )
+} 
+
+export default quote;
